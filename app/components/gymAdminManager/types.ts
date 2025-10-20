3babd6355decd9888701b97b8a303d11067e9517
oxedom <oxedom@gmail.com>
2025-10-20 07:32:51 +0300

    fix: rmeoving

--- app/components/gymAdminManager/types.ts
+++
@@ -1,6 +0,0 @@
-import { UserInstance } from "@guy-vaserman/shared-my-training-app";
-export interface ColumnHandlers {
-  onEditUser: (user: UserInstance) => void;
-  onDeleteUser: (user: UserInstance) => void;
-  onAddUser: () => void;
-}
